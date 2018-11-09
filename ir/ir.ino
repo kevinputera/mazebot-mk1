9170f1a98fe8e0101a3637c63e36dd70ab650ca9
Kevin Putera <kevintiosanna@gmail.com>
2018-11-09 16:54:09 +0800

    added ir testing code

--- ir/ir.ino
+++ ir/ir.ino
@@ -10,11 +10,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-    float x_right = analogRead(IR_RIGHT);
-    float output_right = 2.275427 - (0.00003065916/0.009841547)*(1 - pow(EULER,(0.009841547*x_right)));
+    int output_right = analogRead(IR_RIGHT);
+    Serial.print("RIGHT: ");
     Serial.println(output_right);
 
-    float x_left = analogRead(IR_LEFT);
-    float output_left = 2.208064 - (0.00005070831/0.0100127)*(1 - pow(EULER,(0.0100127*x_left)));
+    int output_left = analogRead(IR_LEFT);
+    Serial.print("LEFT: ");
     Serial.println(output_left);
 }
